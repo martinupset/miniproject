a0e1e452b40b617a30361445e7229e96402fbdbc
martinupset <Martin_Ma@epam.com>
2020-03-10 17:23:29 +0800

    this is for new fn

--- packages/mini-fe/src/pages/Dashboard/index.js
+++ packages/mini-fe/src/pages/Dashboard/index.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react'
 import 'antd/dist/antd.css'
 import {Input, Button, List, Skeleton, Alert} from 'antd'
+import {Modal} from 'antd'
 const {delCookie} = require('../handlecookie')
 
 class Dashboard extends Component{
+  state = {visible: false}
+
   constructor(props){
     super(props);
-    console.log(this.props)
+    console.log(this.state)
   }
+
+  showModal = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+
+  handleOk = e => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
+
+  handleCancel = e => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
 
   logout(){
     this.props.history.push('/login');
@@ -33,13 +56,23 @@ class Dashboard extends Component{
         <List
             bordered
             dataSource={this.props.dashboard.list}
-            renderItem={(item,index)=><List.Item actions={[<a key="list-loadmore-edit">edit</a>,
+            renderItem={(item,index)=><List.Item actions={[<a key="list-loadmore-edit" onClick = {this.showModal}>edit</a>,
             <a key="list-loadmore-more" onClick={()=>alert(`This stuff was update at ${item.updateAt}`)}>more</a>,
             <a key="list-loadmore-delete" onClick={()=>this.props.deleteItemAction(index,item.id)}>delete</a>]}>
               <List.Item.Meta description = {item.status} title = {item.description} />
               </List.Item>
             }>
             </List>
+
+        <Modal
+          title="Basic Modal"
+          visible={this.state.visible}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+        >
+          <p>edit contents here...</p>
+          <p>edit status here...</p>
+        </Modal>
         </div>
 
         <div>
